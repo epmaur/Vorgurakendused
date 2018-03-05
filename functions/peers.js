cfe5892c8b75ebf46d50cc45836b0db09ba3a22d
Epu <eppmauring@gmail.com>
2018-03-05 03:05:24 +0200

    something works

--- functions/peers.js
+++ functions/peers.js
@@ -3,22 +3,6 @@ const request = require('request');
 function getPeers(port) {
   let filename = './peers-'+ port +'.json';
   return require(filename);
-  /*
-  return new Promise(function(resolve, rej) {
-    request.get({
-      uri: url
-        },
-      function(req, res, body) {
-        // console.log('PEERS req:', req, ', res:', res);
-        // resolve(res);
-        resolve(['http://192.168.0.15:1215/a']);
-      }).on('error', function(err) {
-        console.log('PEERS Something happend');
-      })
-  })
-
-  var data = '[{"ip" : "127.0.0.0.1:7070"},{"ip" : "127.0.0.0.1:10100"}]';
-  return data;*/
 }
 
 module.exports.getPeers = getPeers;
--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const sR = require('./functions/post');
 const fw = require('./functions/forward');
 const pe = require('./functions/peers');
 const ipaddr = require('ipaddr.js');
+const fs = require("fs");
+
 
 const LAZINESS = 0.5;
 let PORT = null;
 const PEER_DESTINATION = 'http://192.168.3.11:1215/getpeers';
 const INTERNAL = 'http://localhost:1215/file';
-
+let ID = null;
 let unhandledResponses = {};
 let START_ID = 122;
 let DESTINATIONS = ['http://192.168.0.15:1215/download'];
@@ -62,50 +64,45 @@ function ipAddressHandler(req) {
   }
 }
 
+function checkPeer(url, ip, callback) {
+    const request = require('request');
+    let obj = {};
+    request
+        .get(url)
+        .on('error', function () {
+            obj.ip = ip;
+            obj.available = 'false';
+            JSON.stringify(obj);
+            callback(obj);
+        })
+        .on('response', function() {
+            obj.ip = ip;
+            obj.available = 'true';
+            JSON.stringify(obj);
+            callback(obj);
+        });
+}
+
 function handleNewDestinations() {
-  //console.log('Pulled new destinations');
-  //console.log('DESTINATIONS:', DESTINATIONS);
-  DESTINATIONS = [];
-  var peers = pe.getPeers(PORT);
-  var ping = require('ping');
-
-  const request = require('request');
-  for(let i = 0; i < 1; i++) {
-        let item = peers[i].ip;
-        request('http://localhost:7070/check', { json: true }, function(err, res, body){
-            if (err) { return console.log(err); }
-            console.log('body',body);
-            console.log('res', res);
+    const fs = require('fs');
+    DESTINATIONS = [];
+    let peers = pe.getPeers(PORT);
+    fs.writeFileSync('functions/peers-' + PORT + '.json', JSON.stringify(null));
+    for (let i = 0; i < peers.length; i++) {
+        let ip = peers[i].ip;
+        let url = 'http://' + ip + '/check';
+        checkPeer(url, ip, function (response) {
+            fs.readFile('functions/peers-' + PORT + '.json', function (err, data) {
+                let json = JSON.parse(data);
+                if (json === null) {
+                    json = []
+                }
+                json.push(response);
+                console.log(json);
+                fs.writeFileSync('functions/peers-' + PORT + '.json', JSON.stringify(json));
+            });
         });
-        /*
-        ping.sys.probe(item, function(isAlive){
-            var msg = isAlive ? 'host ' + item + ' is alive' : 'host ' + item + ' is dead';
-        });
-        */
-
     }
-  /*
-  for(let i = 0; i < 2; i++) {
-      // let item = resolve[Math.floor(Math.random() * 2)];
-      let item = peers[i].ip;
-      if (!DESTINATIONS.includes(item)) {
-          DESTINATIONS.push(item);
-      }
-  }
-  */
-
-  /*
-  pe.getPeers(PEER_DESTINATION).then(function(resolve, reject) {
-    //console.log('resolve:', resolve);
-    for(let i = 0; i < 3; i++) {
-      // let item = resolve[Math.floor(Math.random() * 2)];
-      let item = resolve[0];
-      if (!DESTINATIONS.includes(item)) {
-        DESTINATIONS.push(item);
-      }
-    }
-  });
-  */
 }
 
 function handleRequest(request, response){
@@ -148,17 +145,54 @@ function addIdsToDestination (param, urlparam) {
   })
 }
 
+function createFileForNewPeer(port) {
+    fs.open('functions/peers-' + port + '.json', 'w', function () {
+        let json = [];
+        let obj = {};
+        obj.ip = "localhost:7070";
+        obj.available = "false";
+        json.push(obj);
+        obj = {};
+        obj.ip = "localhost:1253";
+        obj.available = "false";
+        json.push(obj);
+        fs.writeFileSync('functions/peers-' + port + '.json', JSON.stringify(json));
+    });
+}
+
+
+function makeDownloadRequest(port, requestedUrl) {
+    const request = require('request');
+    console.log('makedownloadrequest');
+    let peers = pe.getPeers(port);
+    ID =  Math.floor((Math.random() * 1000) + 1);
+    for (let i = 0; i < peers.length; i++) {
+        if (peers[i].available === 'true') {
+            let url = 'http://' + peers[i].ip + '/download?url=' + requestedUrl + '&id=' + ID +'';
+            console.log(url);
+            request
+                .get(url)
+                .on ('response', function (response) {
+                    console.log('response', response);
+                })
+                .on ('error', function (error) {
+                    console.log('error', error);
+                });
+
+        }
+    }
+}
+
 function startServer(port) {
     dispatcher.onGet('/download', function (req, res) {
+        console.log('got a download request');
         if (downloadOrNot() || DESTINATIONS.length === 0) {
             console.log('Starting download...');
             triggerDownloader(req, res).then(function (resolve, reject) {
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify(resolve));
                 //sR.post('http://' + ipAddressHandler(req) +':1215/file?id=' + returnIdParam(req), resolve).then(function(resolve, reject) {
-                sR.post('http://localhost:1215/file?id=' + returnIdParam(req), resolve).then(function (resolve, reject) {
-
-                    console.log('Download was complete')
+                sR.post('http://localhost:1253/file?id=' + returnIdParam(req), resolve).then(function (resolve, reject) {
                 });
                 console.log('Download to client was ok');
             });
@@ -175,13 +209,14 @@ function startServer(port) {
 
     dispatcher.onGet('/check', function (req, res) {
         console.log('checkRequest', req.connection.remoteAddress);
-        /*
-        res.setHeader('Content-Type', 'application/json');
-        sR.post('http://localhost:1215/', 'all good').then(function (resolve, reject) {
-        });
-        res.end(JSON.stringify(resolve));
-        */
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('OK');
+    });
 
+    dispatcher.onGet('/startdownload', function(req, res) {
+        let url = returnUrlParam(req);
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        makeDownloadRequest(port, url);
     });
 
     dispatcher.onPost('/file', function (req, res) {
@@ -194,19 +229,24 @@ function startServer(port) {
             });
             console.log('Return post request');
             delete unhandledResponses[postId];
+        } else if(postId !== null && parseInt(postId) === parseInt(ID)) {
+            console.log('yayy minu request saadeti tagasiiiii');
         } else {
             console.log('Some twat sended me something');
             res.setHeader('Content-Type', 'application/json');
             res.end(JSON.stringify({error: 404}));
         }
     });
+    if (port !== 1253 && port !== 7070) {
+        createFileForNewPeer(port);
+    }
 
     const server = http.createServer(handleRequest);
 
     server.listen(port, function () {
         PORT = port;
         console.log("Server listening on: http://localhost:%s", port);
-        setInterval(handleNewDestinations, 1000);
+        setInterval(handleNewDestinations, 2000);
     });
 }
 
